7461e728ef609437ad97df2d048839c17aa53064
mdharrison1234 <mdharrison@ufl.edu>
2025-04-09 16:26:12 -0400

    Create calculator.py
    Make a basic calculator.

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# Instructions:  Make a basic calculator that can do basic addition, subtraction, multiplication, and division.  Don't forget to account for what happens when you divide by zero!
+# We will get you started.  Please finish the code and then perform a git push. 
+
+
+
+
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
